056bcffa96ed7447d18f196faadcae7b3c793b59
cesarao23 <ildefonso.orozco@actdigital.com>
2025-10-22 16:50:10 -0600

    Texto modificado

--- react-first-steps-fin-seccion-04/src/main.tsx
+++ react-first-steps-fin-seccion-04/src/main.tsx
@@ -5,7 +5,7 @@ import { MyAwesomeApp } from "./MyAwesomeApp";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <strong>Rama de desarrollo test</strong>
+    <strong>Rama de desarrollo test -> Soy creado apartir de test develop</strong>
     <FirstStepsApp />
     <MyAwesomeApp />
   </StrictMode>
