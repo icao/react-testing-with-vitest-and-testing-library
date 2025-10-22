0fa17a7021093b36a203fb8f351e2d805a7dc9de
César Aparicio <clark012389@hotmail.com>
2025-10-22 16:46:48 -0600

    cambios de develop texto

--- react-first-steps-fin-seccion-04/src/main.tsx
+++ react-first-steps-fin-seccion-04/src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { FirstStepsApp } from './FirstStepsApp';
-import { MyAwesomeApp } from './MyAwesomeApp';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { FirstStepsApp } from "./FirstStepsApp";
+import { MyAwesomeApp } from "./MyAwesomeApp";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <strong>Rama de desarrollo test</strong>
     <FirstStepsApp />
     <MyAwesomeApp />
   </StrictMode>
